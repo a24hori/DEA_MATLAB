6bf80ace3cc6dbfbffcbcb47f4ed63bcaae59912
Atsushi Hori <atsushi@ATSH-MBP.local>
2017-02-07 16:15:52 +0900

    Ver. 1.0.0

--- DEA_report/DEA.m
+++ DEA_report/DEA.m
@@ -4,22 +4,36 @@
 % The data matrix should be given inputs first then outputs
 % The number of inputs and outputs should be given in the variables
 % "inputs" and "outputs", respectively.
-    4881,1071,619,1287,591,97,546;
-    4744,956,637,1240,609,107,608;
-    4806,952,544,1234,518,56,497;
-    4810,964,583,1230,559,77,533;
-    4747,910,499,1199,475,104,468;
-    4914,1063,684,1338,649,118,560;
-    4828,907,594,1234,565,82,573;
-    4797,961,519,1203,497,62,435;
-    4838,1049,572,1205,548,67,434;
-    4813,1001,500,1180,473,60,463;
-    4789,1149,506,1171,475,59,503];
-m = 2; % # of inputs
-s= 5; % # of outputs
-n=size(D,2); % # of DMUs
-X = D(1:m,:); % input matrix
-Y = D(m+1:m+s,:); % output matrix
+function [] = DEA(D,inputs,outputs,p)
+% an example 
+% D=[4795,1076,619,1277,585,132,559;
+%     4881,1071,619,1287,591,97,546;
+%     4744,956,637,1240,609,107,608;
+%     4806,952,544,1234,518,56,497;
+%     4810,964,583,1230,559,77,533;
+%     4747,910,499,1199,475,104,468;
+%     4914,1063,684,1338,649,118,560;
+%     4828,907,594,1234,565,82,573;
+%     4797,961,519,1203,497,62,435;
+%     4838,1049,572,1205,548,67,434;
+%     4813,1001,500,1180,473,60,463;
+%     4789,1149,506,1171,475,59,503];
+if (p)
+	D=D.';
+    n=size(D,2); % # of DMUs
+    m=size(D,1); % # of classes
+    X = D(1:inputs,:); % input matrix
+    Y = D(inputs+1:inputs+outputs,:); % output matrix
+else
+    n=size(D,1); % # of DMUs
+    m=size(D,2); % # of classes
+    X = D(:,1:inputs); % input matrix
+    Y = D(:,inputs+1:inputs+outputs); % output matrix
+end
+if (inputs+outputs~=m)
+    fprintf('The sum of inputs and outputs should be equal to the number of DMUs.\n');
+    return;
+end
 % Solve LPO
 uvk = [];
 fvalk = [];
@@ -27,16 +41,28 @@ lambdak = [];
 % uv are separated by: u is coef. of x and v is coef. of y
 
 for i=1:n
-    [uv,fval,exitflag,data,lambda] = linprog(-[zeros(m,1);Y(:,i)],[-X;Y].',zeros(n,1),[X(:,i);zeros(s,1)].',1,zeros(m+s,1));
-    uvk = [uvk uv];
-    fvalk = [fvalk fval];
-    lambdak = [lambdak lambda];
+    % solve DMU i's LP
+    [uv,fval,exitflag,data,lambda] = linprog(-[zeros(inputs,1);Y(:,i)],...
+        [-X;Y].',zeros(n,1),[X(:,i);zeros(outputs,1)].',1,zeros(m,1));
+    uvk = [uvk uv]; % catenate vectors
+    fvalk = [fvalk fval]; % catenate obj. values
+    lambdak = [lambdak lambda]; % catenate dual variables (Lagrange multipliers)
 end
+
 DMU_eff  = [];
+
 for o=1:n
-    DMU_effk = zeros(m+s,1);
+    % compute DMU o's efficient (lambda)
+    DMU_effk = zeros(m,1);
     for j=1:n
         DMU_effk = DMU_effk + lambdak(o).ineqlin(j)*[X(:,j);Y(:,j)];
     end
     DMU_eff = [DMU_eff DMU_effk];
+end
+
+for o=1:n
+    fprintf('DMU %d''s objective\n',o);
+    DMU_eff(:,o)
+end
+
 end
